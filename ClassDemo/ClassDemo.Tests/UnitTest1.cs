f705ef42e2bdd7ced4d12b6355cddd1e8cc70117
rahat15 <rbhatia@ewu.edu>
2024-01-11 15:48:57 -0800

    feat: TDD-Created our first test

--- ClassDemo/ClassDemo.Tests/UnitTest1.cs
+++ ClassDemo/ClassDemo.Tests/UnitTest1.cs
@@ -1,3 +1,5 @@
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
 namespace ClassDemo.Tests;
 
 public class UnitTest1
@@ -5,5 +7,6 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
+        Program.Main();
     }
 }
