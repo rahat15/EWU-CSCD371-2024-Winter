4421aedab03ddbd91df65eba92ddd2f5d4e24161
rahat15 <rbhatia@ewu.edu>
2024-01-22 10:46:09 -0800

    Test: Created Test for ClassName

--- Logger.Tests/LogFactoryTests.cs
+++ Logger.Tests/LogFactoryTests.cs
@@ -5,5 +5,13 @@ namespace Logger.Tests;
 [TestClass]
 public class LogFactoryTests
 {
-
+    [TestMethod]
+    public void CreateLogger_CheckClassName_Success()
+    {
+        LogFactory logFactory = new LogFactory();
+        BaseLogger? fileLogger = logFactory.CreateLogger("FileLogger");
+        
+        if(fileLogger != null)
+            Assert.AreEqual("FileLogger", fileLogger.ClassName);
+    }
 }
