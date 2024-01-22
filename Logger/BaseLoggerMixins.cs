24f028ea1fd34d10c9da88eb17fc4d6c973a811e
rahat15 <rbhatia@ewu.edu>
2024-01-22 11:14:23 -0800

    Errors method created and test pass

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -2,5 +2,17 @@
 
 public static class BaseLoggerMixins
 {
+    public static void Error(this BaseLogger? logger, string message, params object[] args)
+    {
+        if(logger ==  null) 
+        {
+            throw new System.ArgumentNullException(nameof(logger));
+        }
+        else
+        {
+            logger.Log(LogLevel.Error, string.Format(message, args));
+        }
+    }
+
 
 }
