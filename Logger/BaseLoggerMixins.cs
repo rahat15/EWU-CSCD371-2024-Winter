097bc4f3359e8fb9a0563d560805d7a8588f1c21
rahat15 <rbhatia@ewu.edu>
2024-01-22 11:24:09 -0800

    Warning Method created and tests pass

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -14,5 +14,31 @@ public static class BaseLoggerMixins
         }
     }
 
+<<<<<<< Updated upstream
+=======
+    public static void Warning(this BaseLogger? logger, string message, params object[] args)
+    {
+        if (logger == null)
+        {
+            throw new System.ArgumentNullException(nameof(logger));
+        }
+        else
+        {
+            logger.Log(LogLevel.Error, string.Format(message, args));
+        }
+    }
+
+    public static void Debug(this BaseLogger? logger, string message, params object[] args)
+    {
+        if (logger == null)
+        {
+            throw new System.ArgumentNullException(nameof(logger));
+        }
+        else
+        {
+            logger.Log(LogLevel.Error, string.Format(message, args));
+        }
+    }
+>>>>>>> Stashed changes
 
 }
