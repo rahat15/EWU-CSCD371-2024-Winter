ef726fc6b887fa9d01105b7664aa29358b6caf58
Paul <paguilar4@ewu.edu>
2024-01-22 20:07:23 -0800

    Fixed FileLogger Tests
    corrected Date format in FileLogger

--- Logger.Tests/FileLoggerTests.cs
+++ Logger.Tests/FileLoggerTests.cs
@@ -21,7 +21,7 @@ public class FileLoggerTests
     }
 
     [TestMethod]
-    public void Log_WriteLogToFile_Successful()
+    public void Log_FileCreated_Successful()
     {
         // Arrange
         var logger = new FileLogger(_filePath)
@@ -34,6 +34,42 @@ public class FileLoggerTests
 
         // Assert
         Assert.IsTrue(File.Exists(logger.FilePath));
+
+    }
+
+    [TestMethod]
+    public void Log_dWriteLogToFile_Successful()
+    {
+        // Arrange
+        var logger = new FileLogger(_filePath);
+        DateTime currentDate = DateTime.Now;
+        string formattedDate = currentDate.ToString("MM/dd/yyyy");
+
+        // Act
+        logger.Log(LogLevel.Information, "Test message");
+
+        // Assert
+        var logContent = File.ReadAllText(logger.FilePath);
+        StringAssert.Equals(formattedDate, logContent);
+        StringAssert.Equals("FileLogger", logContent);
+        StringAssert.Equals(LogLevel.Information.ToString(), logContent);
+        StringAssert.Equals("Test message", logContent);
+    }
+
+
+    [TestMethod]
+    public void Log_OverwritesExistingLogFile_Successful()
+    {
+        // Arrange
+        var logger = new FileLogger(_filePath);
+
+        // Act
+        logger.Log(LogLevel.Information, "First message");
+        logger.Log(LogLevel.Warning, "Second message");
+
+        // Assert
+        var logContent = File.ReadAllText(logger.FilePath);
+        Assert.AreNotEqual("First message", logContent);
     }
 
 }
--- Logger/FileLogger.cs
+++ Logger/FileLogger.cs
@@ -30,16 +30,21 @@ namespace Logger
             this.FilePath = this.CurrentDirectory + "/" + this.FileName;
 
         }
+        
+       
 
         public override void Log(LogLevel logLevel, string message)
         {
+            DateTime currentDate = DateTime.Now;
+            string formattedDate = currentDate.ToString("MM/dd/yyyy");
+
             if (File.Exists(FilePath))
             {
                 File.Delete(FilePath);
             }
 
             using StreamWriter writeTo = File.AppendText(this.FilePath);
-            writeTo.WriteLine("{0} {1} ", DateTime.Now.ToLongTimeString(), DateTime.Now.ToLongDateString());
+            writeTo.WriteLine("{0} {1} ", formattedDate, DateTime.Now.ToLongTimeString());
             writeTo.WriteLine("{0}", nameof(FileLogger));
             writeTo.WriteLine("{0}: ", logLevel);
             writeTo.WriteLine("{0}", message);
