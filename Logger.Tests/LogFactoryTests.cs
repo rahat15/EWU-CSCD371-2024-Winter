05641178adac54c6458ea59cb7453c3baa870001
Paul <paguilar4@ewu.edu>
2024-01-23 21:41:40 -0800

    Updated LogFactory tests
    created test for configurelogger
    implemented object initializer for _filepath field, now accessed through property

--- Logger.Tests/LogFactoryTests.cs
+++ Logger.Tests/LogFactoryTests.cs
@@ -18,4 +18,14 @@ public class LogFactoryTests
         Assert.IsNotNull(fileLogger, "FileLogger should not be null");
         Assert.AreEqual("FileLogger", fileLogger?.ClassName);
     }
+    [TestMethod]
+    public void ConfigureLogger_CheckFileName_Success() 
+    {  
+        var logFactory = new LogFactory();
+        string filePath = "C:\\path\\to\\your\\test.txt";
+
+
+        logFactory.ConfigureFileLogger(filePath);
+        Assert.AreEqual(filePath, logFactory.FilePath);
+    }
 }
