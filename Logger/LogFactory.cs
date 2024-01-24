b259d309e234c99b628598e28460b97657838155
Paul <paguilar4@ewu.edu>
2024-01-23 21:41:40 -0800

    Updated LogFactory tests
    created test for configurelogger
    implemented object initializer for _filepath field, now accessed through property

--- Logger/LogFactory.cs
+++ Logger/LogFactory.cs
@@ -1,27 +1,41 @@
-﻿namespace Logger;
+﻿using System;
+
+namespace Logger;
 
 public class LogFactory
 {
     private string? _filePath;
 
+    public void ConfigureFileLogger(string filePath)
+    {
+        FilePath = filePath;
+    }
+
+    public string FilePath
+    {
+        get => _filePath!; //telling compiler it's okay to be null in this case
+        set
+        {
+            //if value is null throw exception
+            _filePath = value ?? throw new ArgumentNullException(nameof(value));
+        }
+    }
+
     public BaseLogger? CreateLogger(string className)
     {
-        ConfigureFileLogger(_filePath!);
-        if (_filePath == null)
+        ConfigureFileLogger(FilePath);
+        if (FilePath == null)
         {
             return null;
         }
         else
         {
-            FileLogger logger = new(_filePath) { ClassName = className };
+            FileLogger logger = new(FilePath) { ClassName = className };
 
             return logger;
         }
 
     }
 
-    public void ConfigureFileLogger(string filePath)
-    {
-        _filePath = filePath;
-    }
+
 }
