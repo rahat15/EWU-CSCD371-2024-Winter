d5863d05b716471a65b669bf8f8a65b45d3dffe3
rahat15 <rbhatia@ewu.edu>
2024-01-24 23:31:35 -0800

    Mixins fixed

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -6,19 +6,43 @@ public static class BaseLoggerMixins
 {
     public static void Error(this BaseLogger? logger, string message, params object[] args)
     {
-        if (logger == null)
+        if (logger != null)
         {
-            throw new System.ArgumentNullException(nameof(logger));
+            logger.Log(LogLevel.Error, message);
         }
         else
         {
-            logger.Log(LogLevel.Error, string.Format(CultureInfo.CurrentCulture, message, args));
-            string formatting = string.Format(message, args);
-            //logger.Log(LogLevel.Error, formatting);
+            throw new System.ArgumentNullException(null, "Error mixin got null");
         }
 
     }
     public static void Warning(this BaseLogger? logger, string message, params object[] args)
+    {
+        if (logger != null)
+        {
+            logger.Log(LogLevel.Warning, message);
+        }
+        else
+        {
+            throw new System.ArgumentNullException(null, "Warning mixin got null");
+        }
+        
+    }
+
+    public static void Debug(this BaseLogger? logger, string message, params object[] args)
+    {
+        if (logger != null)
+        {
+            logger.Log(LogLevel.Debug, message);
+        }
+        else
+        {
+            throw new System.ArgumentNullException(null, "Debug mixin got null");
+        } 
+   
+    }
+
+    public static void Information(this BaseLogger? logger, string message, params object[] args)
     {
         if (logger != null)
         {
@@ -27,35 +51,6 @@ public static class BaseLoggerMixins
         else
         {
             throw new System.ArgumentNullException(null, "Information mixin got null");
-        }
-        
-    }
-
-    public static void Debug(this BaseLogger? logger, string message, params object[] args)
-    {
-        if (logger == null)
-        {
-            throw new System.ArgumentNullException(nameof(logger));
-        }
-        else
-        {
-            logger.Log(LogLevel.Debug, string.Format(CultureInfo.CurrentCulture, message, args));
-            string formatting = string.Format(message, args);
-            //logger.Log(LogLevel.Debug, formatting);
-        }
-    }
-
-    public static void Information(this BaseLogger? logger, string message, params object[] args)
-    {
-        if (logger == null)
-        {
-            throw new System.ArgumentNullException(nameof(logger));
-        }
-        else
-        {
-            logger.Log(LogLevel.Information, string.Format(CultureInfo.CurrentCulture, message, args));
-            string formatting = string.Format(message, args);
-            //logger.Log(LogLevel.Information, formatting);
         }
     }
 
